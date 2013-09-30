a144d206f3a7bec939f75b6395a11d1b3a2e1664
Jeff Currier <jcurrier@twitter.com>
2013-09-29 18:16:40 -0700

    Update app names.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "newApp"
+  val appName         = "pinelake"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
